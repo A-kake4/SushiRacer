294721c2d9a0a4a2ba1abb6ff6fa54a0914f1b57
辻省吾 <tsuji.shogo.30047@gmail.com>
2025-08-21 18:49:13 +0900

    ・わさびシールを貼る演出作成。 ・UI配置微調整

--- SushiRacer/Assets/Tsuji_Folder/Programs/RacerProgress_Tsuji.cs
+++ SushiRacer/Assets/Tsuji_Folder/Programs/RacerProgress_Tsuji.cs
@@ -8,6 +8,8 @@ public class RacerProgress_Tsuji : MonoBehaviour
     private float totalDistance;
     private int rank;
     private int beforeRank;
+    [SerializeField]
+    private float wasabiSealPercentage;
 
     private Transform targetObject; // 参照元のオブジェクト
     [SerializeField]
@@ -19,10 +21,22 @@ public class RacerProgress_Tsuji : MonoBehaviour
     private Image playerRankImage; // プレイヤー順位用のUIイメージ
 
     [SerializeField]
+    private Image playerWasabiSeal; // プレイヤーのわさびシール用UIイメージ
+
+    [SerializeField]
     private Sprite rank1Sprite; // １位のスプライト
 
     [SerializeField]
     private Sprite rank2Sprite; // ２位のスプライト
+
+    [SerializeField]
+    private Sprite wasabiSealSprite_30Percent; // わさびシールのスプライト、30%
+
+    [SerializeField]
+    private Sprite wasabiSealSprite_50Percent; // わさびシールのスプライト、50%
+
+    [SerializeField]
+    private Sprite wasabiSealSprite_100Percent; // わさびシールのスプライト、100%
 
     private void FixedUpdate()
     {
@@ -38,6 +52,29 @@ public class RacerProgress_Tsuji : MonoBehaviour
             }
         }
         beforeRank = rank;
+
+        if (playerWasabiSeal != null)
+        {
+            // UIを非表示
+            playerWasabiSeal.color = new Color(playerWasabiSeal.color.r, playerWasabiSeal.color.g, playerWasabiSeal.color.b, 1f);
+            if (wasabiSealPercentage >= 100f)
+            {
+                playerWasabiSeal.sprite = wasabiSealSprite_100Percent; // 100%のスプライトを設定
+            }
+            else if (wasabiSealPercentage >= 50f)
+            {
+                playerWasabiSeal.sprite = wasabiSealSprite_50Percent; // 50%のスプライトを設定
+            }
+            else if (wasabiSealPercentage >= 30f)
+            {
+                playerWasabiSeal.sprite = wasabiSealSprite_30Percent; // 30%のスプライトを設定
+            }
+            else
+            {
+                // UIを非表示
+                playerWasabiSeal.color = new Color(playerWasabiSeal.color.r, playerWasabiSeal.color.g, playerWasabiSeal.color.b, 0f);
+            }
+        }
     }
 
     public void UpdateProgress(Transform[] waypoints)
@@ -54,6 +91,11 @@ public class RacerProgress_Tsuji : MonoBehaviour
     public void SetRank(int _rank)
     {
         rank = _rank;
+    }
+
+    public void SetWasabiSealPercentage(float percentage)
+    {
+        wasabiSealPercentage = Mathf.Clamp(percentage, 0f, 100f);
     }
 
     public float GetTotalDistance()

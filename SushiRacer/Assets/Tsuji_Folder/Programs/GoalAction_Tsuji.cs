b766df036d1c62b6fc0f3781e71fb083e1baac47
辻省吾 <tsuji.shogo.30047@gmail.com>
2025-08-21 22:53:31 +0900

    ・ゴールを検出するための壁を作成

---
+++ SushiRacer/Assets/Tsuji_Folder/Programs/GoalAction_Tsuji.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GoalAction_Tsuji : MonoBehaviour
+{
+    private GameObject goalObj = null;
+    [SerializeField]
+    private Camera goalCamera = null;
+
+    [SerializeField]
+    private Camera cameraP1 = null;
+
+    [SerializeField]
+    private Camera cameraP2 = null;
+
+    [SerializeField]
+    private Canvas canvasTsuji = null;
+
+    [SerializeField]
+    private float waitTime = 0.0f;
+
+    private bool onGoalFlag = false;
+
+    private void FixedUpdate()
+    {
+        if(onGoalFlag == true)
+        {
+            canvasTsuji.gameObject.SetActive(false);
+
+            StartCoroutine(CameraChange());
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        int playerLayer = LayerMask.NameToLayer("Player");
+        if (collider.gameObject.layer == playerLayer && goalObj == null)
+        {
+            goalObj = collider.gameObject;
+            onGoalFlag = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        int playerLayer = LayerMask.NameToLayer("Player");
+        if (collision.gameObject.layer == playerLayer && goalObj == null)
+        {
+            goalObj = collision.gameObject;
+            onGoalFlag = true;
+        }
+    }
+
+    IEnumerator CameraChange()
+    {
+        yield return new WaitForSeconds(waitTime);
+        goalCamera.gameObject.SetActive(true); // ÉSÅ[ÉãÉJÉÅÉâÇóLå¯Ç…Ç∑ÇÈ
+        NonActives();
+    }
+
+    void NonActives()
+    {
+        cameraP1.gameObject.SetActive(false);
+        cameraP2.gameObject.SetActive(false);
+    }
+
+    public bool GetGoalFlag()
+    {
+        return onGoalFlag;
+    }
+}
